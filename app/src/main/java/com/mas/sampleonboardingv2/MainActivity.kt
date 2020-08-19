319179b400c3f575e129d5849a4ae7d18f28fc3e
Lava21 <msyarif559@gmail.com>
2020-08-19 10:26:31 +0700

    update commit

--- app/src/main/java/com/mas/sampleonboardingv2/MainActivity.kt
+++ app/src/main/java/com/mas/sampleonboardingv2/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.mas.sampleonboardingv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.slide_item.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +39,78 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         vpIntroSlider.adapter = introSliderAdapter
+        setupIndicators()
+
+        setCurrentIndicator(0)
+
+        vpIntroSlider.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+
+        btnNext.setOnClickListener {
+            if (vpIntroSlider.currentItem + 1 < introSliderAdapter.itemCount){
+                vpIntroSlider.currentItem += 1
+            } else {
+                Intent(applicationContext, HomeActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
+
+        tvSkipIntro.setOnClickListener {
+            Intent(applicationContext, HomeActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+    }
+
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for (i in indicators.indices){
+            indicators[i] = ImageView(applicationContext)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            llIndicator.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int){
+        val childCount = llIndicator.childCount
+        for (i in 0 until childCount){
+            val imageView = llIndicator[i] as ImageView
+
+            if (i == index){
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
     }
 }
